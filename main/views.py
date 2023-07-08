d4ef36c9f77da605c5d4bedad560db62d0729380
echewisi <josephechewisi@gmail.com>
2023-07-09 00:06:15 +0100

    templates|view2

--- main/views.py
+++ main/views.py
@@ -24,14 +24,47 @@ def signup_view(request):
         if form.is_valid():
             user= form.save(commit= False)
             user.password= make_password(form.cleaned_data['password1'])
-            user.email= form.cleaned_data(['email'])
+            user.email= form.cleaned_data['email']
             user.save()
             messages.success(request, "you have signed up!", extra_tags="success!")
             return redirect('login')
     
     else:
         form= CustomUserCreationForm()
+    
+    return render(request, 'signup.html', {'form': form})
 
-
+#if user is already logged in:
+def refer_view(request, referral_code):
+    if request.user.is_authenticated:
+        return redirect('portfolio')
+    try:
+        #get the user profile of the referrer
+        referrer= User.objects.get(profile__referral_code= referral_code)
+    except User.DoesNotExist:
+        #show error message if the referrer does not exist
+        return HttpResponse('Referrer does not exist')
+    
+    if request.method== 'POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            user= form.save(commit=False)
+            user.password= make_password(form.cleaned_data['password1'])
+            user.email=form.cleaned_data['email']
+            user.save()
+            #create a referral instance
+            referral= Referral.objects.create(user=user,  referrer= referrer)
+            referral.save()
+            
+            if referrer is not None:
+                referrer.profile.bonus += 100  #add referral bonus to referrer
+                referrer.profile.save()
+                messages.success(request, f'{referrer.username} received a bonus of 100 points. you signed up with their referral link ')
+            messages.success(request, "you have successfully signed up!")
+            return redirect('login')
+    else:
+        form= UserCreationForm()
+    
+    return render(request, 'signup.html', {'form': form})
 
 # Create your views here.
