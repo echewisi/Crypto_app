8dea92337f420ef79c3374fe89d82b2d5c4bd77b
echewisi <josephechewisi@gmail.com>
2023-10-31 01:59:01 +0100

    minor change

--- Crypto_app/settings.py
+++ Crypto_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #this are the apps
+    #these are the apps
     'main',
     'endpoints',
     #third party apps
