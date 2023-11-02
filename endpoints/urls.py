6a8ca503bea355a807b2111054b142b62ae73ef5
echewisi <josephechewisi@gmail.com>
2023-11-02 15:59:52 +0100

    url changes

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import CryptomodelCreateView, CryptomodelDeleteView, CryptomodelDetailView, CryptomodelListView, ReferralListView, ProfileCreateView, ProfileModelView
 from rest_framework.routers import DefaultRouter
 
@@ -7,10 +7,10 @@ from rest_framework.routers import DefaultRouter
 # router.register(r'cryptomodels', CryptomodelCreateView)
 
 urlpatterns = [
-    path('api/register/', ProfileCreateView.as_view(), name= 'profile-create'), 
-    path('api/profile_user/<int:pk>', ProfileModelView.as_view(), name='profile_view'),
-    path('api/cryptomodels/<str:id_from_API>/', CryptomodelDeleteView.as_view(), name='cryptomodel-delete'),    
-    path('api/referrals/', ReferralListView.as_view(), name='referral-list'),
-    path('api/cryptomodels/', CryptomodelListView.as_view(), name='cryptomodel-list'),
-    path('api/cryptomodels/<str:id_from_API>/', CryptomodelDetailView.as_view(), name='cryptomodel-detail'),
+    path('register/', ProfileCreateView.as_view(), name= 'profile-create'), 
+    path('profile_user/<int:pk>', ProfileModelView.as_view(), name='profile_view'),
+    path('cryptomodels/<str:id_from_API>/', CryptomodelDeleteView.as_view(), name='cryptomodel-delete'),    
+    path('referrals/', ReferralListView.as_view(), name='referral-list'),
+    path('cryptomodels/', CryptomodelListView.as_view(), name='cryptomodel-list'),
+    path('cryptomodel_detail/<str:id_from_API>/', CryptomodelDetailView.as_view(), name='cryptomodel-detail'),
 ]
